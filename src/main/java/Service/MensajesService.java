d8da7373202b46c89e6946476f26b4cefb88b920
Roberto1313 <67878026+robertomarroquin1313@users.noreply.github.com>
2023-07-07 17:49:09 -0600

    Uso de LocalDate.now()
    Esta funcion captura la fecha actual automaticamente para que no tener confictos con  el formato de la base de datos

--- src/main/java/Service/MensajesService.java
+++ src/main/java/Service/MensajesService.java
@@ -4,15 +4,44 @@
  */
 package Service;
 
+import Classes.MensajesDAO;
 import Model.Mensajes;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Scanner;
 
 /**
  *
  * @author Roberto Marroquín
  */
 public class MensajesService {
-    public static void crearMensajeDB(){
+    public static void crearMensajeDB() throws ParseException{
+        Scanner entrada = new Scanner(System.in);
         
+        System.out.print("Escribe tu mensaje: ");
+        String ms = entrada.nextLine();
+        
+        
+        System.out.print("Ingresa tu nombre: ");
+        String nom = entrada .nextLine();
+        
+  
+        
+       //hacemos setting con los datos requeridos
+        Mensajes mensaje = new Mensajes();
+        mensaje.setMensaje(ms);
+        mensaje.setAutorMensaje(nom);
+        mensaje.setFecha( Date.valueOf(LocalDate.now()));
+        //Enviamos el Mensaje a la clase DAO para que ejecute el statement
+        MensajesDAO.crearMensajeDB(mensaje);
+        
+        
+  
+        
+        
+     
     }
     
     public static void listarMensajeDB(){
