66d4a707056c72b4167af9c9dc6f925106a2b107
Roberto1313 <67878026+robertomarroquin1313@users.noreply.github.com>
2023-07-07 17:45:44 -0600

    Funcion crear mensaje

--- src/main/java/Classes/MensajesDAO.java
+++ src/main/java/Classes/MensajesDAO.java
@@ -5,6 +5,10 @@
 package Classes;
 
 import Model.Mensajes;
+import com.mycompany.mensajesapp.Connection.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  *
@@ -14,7 +18,28 @@ public class MensajesDAO extends Mensajes {
     
     
     public static void crearMensajeDB(Mensajes mensaje){
+        Conexion dbConnect = new Conexion();
         
+        
+        try(Connection cone = dbConnect.get_Connection()){
+            
+            PreparedStatement ps = null;
+            try{
+                    String query = "INSERT INTO mensajes (mensaje, autorMensaje,fechaMensaje) VALUES (?,?,?);";
+                    ps=cone.prepareStatement(query);
+                    ps.setString(1, mensaje.getMensaje());
+                    ps.setString(2, mensaje.getAutorMensaje());
+                    ps.setDate(3, mensaje.getFecha());
+                    ps.executeUpdate();
+                    System.out.println("\nEl mensaje se creó correctamente");
+            }catch(SQLException e){
+                System.out.println(e);
+            }
+            
+            
+        }catch(SQLException e){
+            System.out.println(e);
+        }
     }
     public static void leerMensajeDB(){
         
